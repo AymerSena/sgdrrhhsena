a2dd81da27d604bf1983cf3f04ef178007e04857
Aymer Sena <aycruz04@misena.edu.co>
2022-04-03 14:42:51 -0500

    Se eliminaron algunas validaciones que no hacen nada

--- photoPerfil.php
+++ photoPerfil.php
@@ -15,7 +15,7 @@
             <nav>
                 <ul>
                     <li><img src="https://1.bp.blogspot.com/-MeCxaLO8njU/YT5yRMu7KrI/AAAAAAAAAHI/NhoZIlmquMUWDoiVjAzF3nTF1WnwqRTSQCNcBGAsYHQ/s0/descarga.png" id="photoPer"></li>
-                    <li> <img src="https://1.bp.blogspot.com/-EO10WM7B0Ig/YT5yOT5S7JI/AAAAAAAAAGw/FfqaAQ19Y709UTCC9jBUt7CW9pEv8_xjACNcBGAsYHQ/s0/IconoNotificaciones.png" id="photoNoti"></li>
+                    <li><img src="https://1.bp.blogspot.com/-EO10WM7B0Ig/YT5yOT5S7JI/AAAAAAAAAGw/FfqaAQ19Y709UTCC9jBUt7CW9pEv8_xjACNcBGAsYHQ/s0/IconoNotificaciones.png" id="photoNoti"></li>
                     <li><button>Gestion contraseña</button></li>
                     <li><button>Cerrar sesión</button></li>
                 </ul>
