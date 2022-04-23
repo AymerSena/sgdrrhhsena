5100a6c3fff48f35f8ba5321afef571df38fce50
AymerSena <87399535+AymerSena@users.noreply.github.com>
2022-04-22 19:56:41 -0500

    Pruebas con la interfaz grafica realizada por David, integracion al menuAdministrador.

--- menuAdministrador.php
+++ menuAdministrador.php
@@ -13,33 +13,39 @@ require("sesionAdmin.php");
 </head>
 
 <body>
-<?php 
+<!-- Desde acá -->
+<?php
+    include("cabecera.php")
+    
+/*
                     require("conexionBD.php");
                     $documentoFunLoguea=1110000001;/// Esta variable debe capturar quien se logueo por el momento se pone manual.
-                    $query=$conexion->query("SELECT * FROM tblusuario WHERE UsuCedula='$documentoFunLoguea'");
+                    $conUrl="SELECT UsuFotoPerfil FROM tblusuario WHERE UsuCedula=1110000001";
+                    $query=$conexion->query($conUrl);
                     while ($row = $query->fetch_assoc()) {
                         $return = $row['UsuFotoPerfil'];
                     }
                     if ($return==NULL){
                         $return="https://1.bp.blogspot.com/-MeCxaLO8njU/YT5yRMu7KrI/AAAAAAAAAHI/NhoZIlmquMUWDoiVjAzF3nTF1WnwqRTSQCNcBGAsYHQ/s0/descarga.png";
                     }
-                    ?>
+*/
+?>
 <div class="cuerpo">
+    <!--
         <div class="panelLeft">
             <nav>
                 <ul>
                     <li><a href="photoPerfil.php"><img src="<?php echo $return; ?>" id="photoPer"></a></li>
                     <li> <img src="https://1.bp.blogspot.com/-EO10WM7B0Ig/YT5yOT5S7JI/AAAAAAAAAGw/FfqaAQ19Y709UTCC9jBUt7CW9pEv8_xjACNcBGAsYHQ/s0/IconoNotificaciones.png" id="photoNoti"></li>
                     <li><button>Gestion contraseña</button></li>
-                    <form action="" method="POST">
-                    <li><button name="cerrar_sesion">Cerrar sesión</button></li>
-                    </form>
+                    <li><button><a href="index.php">Cerrar sesión</a></button></li>
                 </ul>
             </nav>
         </div>
         <div class="titlePag">
             <h1>Titulo de la página va acá</h1>
         </div>
+        -->
         <div class="container">
         <div id="Menu">
         <h1 id="tit">¿Que deseas hacer?</h1>
@@ -49,16 +55,16 @@ require("sesionAdmin.php");
                     <a href="crearUsuario.php"><img class="logo" src="https://1.bp.blogspot.com/--_3Twc7jE_4/YT5yHKJ8sqI/AAAAAAAAAF0/ywykiJsi3yY8S1op1g-MyU8O072GJoeYACNcBGAsYHQ/s0/IconoAdmPerfiles.png"></a>
                 </li>
                 <li>
-                    <a href=""><img class="logo" src="https://1.bp.blogspot.com/-pWbP6N_2-F0/YT5yIQZXEtI/AAAAAAAAAGA/n0-4cvp7gFYTvNkgiiwsFx1FQM-5hRWZACNcBGAsYHQ/s320/IconoCarDesArch.png"></a>
+                    <a href="carge_documentos.php"><img class="logo" src="https://1.bp.blogspot.com/-pWbP6N_2-F0/YT5yIQZXEtI/AAAAAAAAAGA/n0-4cvp7gFYTvNkgiiwsFx1FQM-5hRWZACNcBGAsYHQ/s320/IconoCarDesArch.png"></a>
                 </li>
                 <li>
                     <a href=""><img class="logo" src="https://1.bp.blogspot.com/-yxwhRnbY8mQ/YT5yLdXGtOI/AAAAAAAAAGY/2MsyxUcL5uAVrLU0RpV2-lZvznBtNf4CgCNcBGAsYHQ/s320/IconoCrudDataSen.png"></a>
                 </li>
                 <li>
-                    <a href="SolicitudEmpleado.php"><img class="logo" src="https://1.bp.blogspot.com/-CVbG8VMJUGg/YT5yP3LJHnI/AAAAAAAAAG8/oSqBcKNbMWcH6gM4eXFSKPc2TC3Mm0rJgCNcBGAsYHQ/s320/IconoSolicitarYConsuVa.png"></a>
+                    <a href="NegociacionPeriodos.php"><img class="logo" src="https://1.bp.blogspot.com/-CVbG8VMJUGg/YT5yP3LJHnI/AAAAAAAAAG8/oSqBcKNbMWcH6gM4eXFSKPc2TC3Mm0rJgCNcBGAsYHQ/s320/IconoSolicitarYConsuVa.png"></a>
                 </li>
                 <li>
-                    <a href="datosBasicos.html"><img class="logo" src="https://1.bp.blogspot.com/-a5TanyEIRgk/YT5yHI4qFcI/AAAAAAAAAFs/mCLSeiyQ5RMHRy8tevTtSORRIaODcvaFQCNcBGAsYHQ/s320/IconoActualiData.png"></a>
+                    <a href="datosBasicos.php"><img class="logo" src="https://1.bp.blogspot.com/-a5TanyEIRgk/YT5yHI4qFcI/AAAAAAAAAFs/mCLSeiyQ5RMHRy8tevTtSORRIaODcvaFQCNcBGAsYHQ/s320/IconoActualiData.png"></a>
                 </li>
                 <li>
                     <a href="menuInformes.php"><img class="logo" src="https://1.bp.blogspot.com/-zNm1SxTs37c/YT5yN-a4IwI/AAAAAAAAAGs/ya7hx4ayapcuP53sZUm-XyRiY27bjjnIACNcBGAsYHQ/s0/IconoInformes.png"></a>
@@ -68,20 +74,15 @@ require("sesionAdmin.php");
                 </li>
             </ul>
         </nav>
-        <form action="" method="POST">
-            <button name="claro">Cerrar</button>
-        </form>
         <img src="https://1.bp.blogspot.com/-CRGFBvE8s8k/YT5yRhIEj8I/AAAAAAAAAHM/dplt4qgxJmcjfSP213rWRyF0EoW_BQlSACNcBGAsYHQ/s332/logoPag.png" id="logoUnix">
     </div>
         </div>
     </div>
+    <?php
+    include("pie.php")
+    ?>
+   <!-- Hasta acá -->
 
-    <div class="foo">
-        <footer>
-            <h2>Pie de página va acá</h2>
-        </footer>
-    </div>
- 
 </body>
 
 </html>
