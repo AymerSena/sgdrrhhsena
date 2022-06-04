beb7b934e63c03e91dd9b2e9cde406db8dfdacf6
AymerSena <87399535+AymerSena@users.noreply.github.com>
2022-06-03 21:23:46 -0500

    Se creo la clase evaluaciones para empezar a trabajar en objetos

---
+++ classphp/evaluaciones.php
@@ -0,0 +1,39 @@
+<?php
+require_one("./conexionBD.php");
+class evaluaciones{
+
+    public function construct__($eNam,$eDes,$eAre){
+        private $idEval = insertEval();
+        public $nombreEva;
+        public $descriEva;
+        public $keyArea;
+     $nombreEva = $this -> eNam;
+     $descriEva = $this -> eDes;
+     $keyArea = $this -> eAre;
+    }
+
+    public function insertEval(){
+     $this -> nombreEva;
+     $this -> descriEva;
+     $this -> keyArea;
+
+        $sql = "INSERT INTO tblevaluaciones VALUES (NULL, '$nombreEva','$nombreEva','$descriEva','$keyArea')";
+        $runInsert = $conexion ->query($sql);
+        $querId="SELECT EvaCodigo FROM tblevaluaciones WHERE EvaCodigo=(SELECT max(EvaCodigo) FROM tblevaluaciones)";
+        $result=$conexion ->query($querId);
+        return $result;
+    }
+
+    public function searchEval($a){
+        $sql = "SELECT * FROM tblevaluaciones WHERE EvaForAre = '$a'";
+        $runSelect = $conexion -> query($sql);
+        return $runSelect;
+    }
+
+    public function updateEval($a,$b, $c, ){´
+    
+    }
+
+}
+
+?>
